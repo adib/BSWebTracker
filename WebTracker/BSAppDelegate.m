eb7b974bc656a336e3553b046e611c4fa67248ff
Sasmito Adibowo <adib@basil-salad.com>
2013-04-27 21:47:46 +0800

    Initial Commit

---
+++ WebTracker/BSAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BSAppDelegate.m
+//  WebTracker
+//
+//  Created by Sasmito Adibowo on 27-04-13.
+//  Copyright (c) 2013 Basil Salad Software. All rights reserved.
+//
+
+#import "BSAppDelegate.h"
+
+@implementation BSAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ WebTracker/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WebTracker
+//
+//  Created by Sasmito Adibowo on 27-04-13.
+//  Copyright (c) 2013 Basil Salad Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
